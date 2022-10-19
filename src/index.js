a329b2c5b8db5084aaedf0a0611b52928bec446b
Hannes Kamecke <hannes.kamecke@vt.atlascopco.com>
2022-10-19 11:47:52 +0200

    Open serial port

--- src/index.js
+++ src/index.js
@@ -17,7 +17,14 @@ async function runUSB() {
     }
 }
 
+
+
+let cleanupSerial = null;
 async function runSerial() {
+    if (cleanupSerial) {
+        cleanupSerial();
+        cleanupSerial = null;
+    }
     const out = document.getElementById('outSerial');
     try {
         out.innerHTML = '';
@@ -32,6 +39,31 @@ async function runSerial() {
             'usbVendorId',
             'usbProductId',
           ].map(k => k + ': ' + port[k]).join('\n');
+
+        const form = document.getElementById('serial-open');
+        const openButton = document.getElementById('serialOpen');
+        form.style.display = 'block';
+        const openPort = async () => {
+            try {
+                const serialOptions = {
+                    baudRate: +document.getElementById('baudRate').value,
+                    dataBits: +document.getElementById('dataBits').value,
+                    stopBits: +document.getElementById('stopBits').value,
+                    parity: document.getElementById('parity').value,
+                };
+                console.log(serialOptions);
+                await port.open(serialOptions);
+                out.innerHTML += '\nopened';
+            }
+            catch (e) {
+                out.innerHTML = e.message;
+            }
+        };
+        openButton.addEventListener('click', openPort);
+        cleanupSerial = () => {
+            cleanupSerial && cleanupSerial();
+            openButton.removeEventListener('click', openPort);
+        };
     }
     catch (e) {
       out.innerHTML = e.message;
